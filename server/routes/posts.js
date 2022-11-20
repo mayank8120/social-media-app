f91fb3f36181ff24203664324ecdec22599c6685
mayank8120 <mayanklaxkar11@gmail.com>
2022-11-20 23:29:23 +0530

    First Checkpoint

---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { createPost, deletePost, getPosts, likePost, updatePost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likepost', likePost);
+
+export default router;
